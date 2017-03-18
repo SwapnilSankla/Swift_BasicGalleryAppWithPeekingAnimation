d3820128e7cfa66eb1e2d1b223aefee0a887f2c5
Swapnil Sankla <sanklas@thoughtworks.com>
2017-03-18 16:38:48 +0530

    Initial Commit

---
+++ Gallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Swapnil Sankla on 18/03/17.
+//  Copyright © 2017 Swapnil Sankla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
