bed3e2e0124386b2e89cd6a84bc03dacdd71ee42
Swapnil Sankla <sanklas@thoughtworks.com>
2017-04-09 10:02:29 +0530

    Bounce working

--- Gallery/ViewController.swift
+++ Gallery/ViewController.swift
@@ -1,25 +1,49 @@
-//
-//  ViewController.swift
-//  Gallery
-//
-//  Created by Swapnil Sankla on 18/03/17.
-//  Copyright © 2017 Swapnil Sankla. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageStackView: UIStackView!
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    let imagePickerController = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let uiImage = info["UIImagePickerControllerOriginalImage"] as! UIImage
+        let imageView = UIImageView(image: uiImage)
+        let heightConstraint =
+            NSLayoutConstraint(item: imageView,
+                               attribute: .height,
+                               relatedBy: .equal,
+                               toItem: nil,
+                               attribute: .notAnAttribute,
+                               multiplier: 1,
+                               constant: 200)
+        let widthConstraint =
+            NSLayoutConstraint(item: imageView  ,
+                               attribute: .width,
+                               relatedBy: .equal,
+                               toItem: nil,
+                               attribute: .notAnAttribute,
+                               multiplier: 1,
+                               constant: 200)
+        imageView.addConstraint(heightConstraint)
+        imageView.addConstraint(widthConstraint)
+        imageStackView.addArrangedSubview(imageView)
+        imagePickerController.dismiss(animated: true, completion: nil)
+        view.layoutIfNeeded()
     }
 
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        imagePickerController.dismiss(animated: true, completion: nil)
+    }
 
+    @IBAction func browseButtonTapped(_ sender: AnyObject) {
+        present(imagePickerController, animated: true, completion: nil)
+    }
 }
-
